0c3141f0ee4b941a4c609eedfbc2c159866f0ca7
Joel Baxter <joel.eliot.baxter@gmail.com>
2018-09-11 17:14:11 +0100

    Update yearly_purchases.rb

--- yearly_purchases.rb
+++ yearly_purchases.rb
@@ -1,7 +1,3 @@
-#This is a codewars 6kyu challenge: define a best_monthly method for a set of users, provided as an array with a group of users for each month.
-#You must return a nested array of all users who made a purchase in every month, in the format [count, [all, users], next count, [next, users]], sorted by most purchases to least purchases.
-#I added a few extras, by defining a YearlyUsers class and adding a few other basic methods: returning a list of all users who made purchases, and returning the purchase count for a specific user.
-
 class YearlyUsers
 
   def initialize(*users)
